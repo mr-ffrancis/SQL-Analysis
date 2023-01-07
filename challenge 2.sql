383fa9ec648e68a999723a5964521a5dd390c7b4
mr_ffrancis <adiotunji@gmail.com>
2023-01-07 15:27:48 +0100

    UPDATED

--- challenge 2.sql
+++ challenge 2.sql
@@ -1,114 +1,267 @@
-
 use pizza_runner;
-DROP TABLE IF EXISTS runners;
-CREATE TABLE runners (
-  runner_id INTEGER,
-  registration_date DATE
-);
-INSERT INTO runners
-  (runner_id, registration_date)
-VALUES
-  (1, '2021-01-01'),
-  (2, '2021-01-03'),
-  (3, '2021-01-08'),
-  (4, '2021-01-15');
+
+show tables;
+ -- Preprocessing 
+	UPDATE runner_orders 
+SET 
+    cancellation = NULL
+WHERE
+    cancellation = '';
+    -- Updating customer orders and adjusting the pizza serial number to 1 and 2 and Mod 3 for Odd and even order numbers    
+		UPDATE customer_orders 
+SET 
+    pizza_serial_number = 1
+WHERE
+    MOD(order_id, 2) != 0;
+		UPDATE customer_orders 
+SET 
+    pizza_serial_number = 2
+WHERE
+    MOD(order_id, 2) = 0;
+UPDATE customer_orders 
+SET 
+    pizza_serial_number = 3
+WHERE
+    MOD(order_id, 3) = 0;
+	
+    -- Updating customer orders and adjusting the pizza serial number to 1 and 2 and Mod 3 for Odd and even order numbers    
+		UPDATE customer_orders 
+SET 
+    pizza_serial_number = 1
+WHERE
+    MOD(order_id, 2) != 0;
+        
+	-- Insering records to daily_Pizza
+SELECT 
+    *
+FROM
+    daily_pizza;
+        alter table daily_pizza change start_date start_date varchar(20);
+        alter table daily_pizza change finish_date finish_date varchar(20);
+		insert into daily_pizza values (1,1,2000,"1/12/2022 12:03:25","1/12/2022 12:03:25","2,3,4,5",100);
+		insert into daily_pizza values (3,2,4000,"1/12/2022 12:03:25","1/12/2022 12:03:25","2,3,4,5",100);
+        insert into daily_pizza values (2,1,2000,"1/12/2022 12:03:25","1/12/2022 12:03:25","2,3,4,5",100);
+-- --------------------------- --------------------------- --------------------------- --------------------------- ---------------------------
+-- 1 Question 1 How many pizzas were ordered - 14 orders were made?
+SELECT 
+    COUNT(order_id)
+FROM
+    customer_orders;
+
+-- 2 How many unique customer orders were made - 10 Unique Orders were made?
+SELECT 
+    COUNT(DISTINCT order_id)
+FROM
+    customer_orders;
+
+-- 3 How many successful orders were delivered by each runner - 8 orders were successfully delivered?
+SELECT 
+    COUNT(1)
+FROM
+    runner_orders
+WHERE
+    cancellation IS NULL;
+
+-- 4 How many of each type of pizza was delivered?
+SELECT 
+    c.pizza_serial_number,
+    p.pizza_name,
+    COUNT(1) number_of_orders
+FROM
+    customer_orders c
+        LEFT JOIN
+    daily_pizza d ON c.pizza_serial_number = d.pizza_serial_number
+        LEFT JOIN
+    pizza_names p ON d.pizza_id = p.pizza_id
+GROUP BY pizza_serial_number;
 
 
-DROP TABLE IF EXISTS customer_orders;
-CREATE TABLE customer_orders (
-  order_id INTEGER,
-  customer_id INTEGER,
-  pizza_id INTEGER,
-  exclusions VARCHAR(4),
-  extras VARCHAR(4),
-  order_time TIMESTAMP
-);
+-- 5 How many Vegetarian and Meatlovers were ordered by each customer - 10 Meatlovers and 4 vegetarian?
+SELECT 
+    p.pizza_name, COUNT(1) number_of_orders
+FROM
+    customer_orders c
+        LEFT JOIN
+    daily_pizza d ON c.pizza_serial_number = d.pizza_serial_number
+        LEFT JOIN
+    pizza_names p ON d.pizza_id = p.pizza_id
+GROUP BY pizza_name;
 
-INSERT INTO customer_orders
-  (order_id, customer_id, pizza_id, exclusions, extras, order_time)
-VALUES
-  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
-  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
-  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
-  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
-  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
-  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
-  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
-  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
-  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
-  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
-  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
-  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
-  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
-  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+-- 6 What was the maximum number of pizzas delivered in a single order -  3 Pizza were delivered with order number 4?
+SELECT 
+    c.customer_id,
+    c.order_id,
+    COUNT(c.order_id) AS number_of_orders
+FROM
+    customer_orders c
+        LEFT JOIN
+    runner_orders r ON r.order_id = c.order_id
+WHERE
+    r.cancellation IS NULL
+GROUP BY customer_id , order_id
+ORDER BY COUNT(1) DESC;
 
-
-DROP TABLE IF EXISTS runner_orders;
-CREATE TABLE runner_orders (
-  order_id INTEGER,
-  runner_id INTEGER,
-  pickup_time VARCHAR(19),
-  distance VARCHAR(7),
-  duration VARCHAR(10),
-  cancellation VARCHAR(23)
-);
-
-INSERT INTO runner_orders
-  (order_id, runner_id, pickup_time, distance, duration, cancellation)
-VALUES
-  ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
-  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
-  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
-  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
-  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
-  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
-  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
-  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
-  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
-  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+-- 7 For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 
 
-DROP TABLE IF EXISTS pizza_names;
-CREATE TABLE pizza_names (
-  pizza_id INTEGER,
-  pizza_name TEXT
-);
-INSERT INTO pizza_names
-  (pizza_id, pizza_name)
-VALUES
-  (1, 'Meatlovers'),
-  (2, 'Vegetarian');
 
 
-DROP TABLE IF EXISTS pizza_recipes;
-CREATE TABLE pizza_recipes (
-  pizza_id INTEGER,
-  toppings TEXT
-);
-INSERT INTO pizza_recipes
-  (pizza_id, toppings)
-VALUES
-  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
-  (2, '4, 6, 7, 9, 11, 12');
+-- 8 How many pizzas were delivered that had both exclusions and extras - 7 Orders have both extras and exclusions in it?
+SELECT 
+    COUNT(*)
+FROM
+    customer_orders
+WHERE
+    (exclusions <> 0 || extras <> 0);
 
 
-DROP TABLE IF EXISTS pizza_toppings;
-CREATE TABLE pizza_toppings (
-  topping_id INTEGER,
-  topping_name TEXT
-);
-INSERT INTO pizza_toppings
-  (topping_id, topping_name)
-VALUES
-  (1, 'Bacon'),
-  (2, 'BBQ Sauce'),
-  (3, 'Beef'),
-  (4, 'Cheese'),
-  (5, 'Chicken'),
-  (6, 'Mushrooms'),
-  (7, 'Onions'),
-  (8, 'Pepperoni'),
-  (9, 'Peppers'),
-  (10, 'Salami'),
-  (11, 'Tomatoes'),
-  (12, 'Tomato Sauce');
+-- 9 What was the total volume of pizzas ordered for each hour of the day?
+SELECT 
+    EXTRACT(HOUR FROM order_time) Hour, COUNT(1) order_count
+FROM
+    customer_orders
+GROUP BY Hour
+ORDER BY Hour ASC;
+
+-- 10 What was the volume of orders for each day of the week?
+SELECT 
+    DAYNAME(order_time) Days, COUNT(1) order_count
+FROM
+    customer_orders
+GROUP BY Days
+ORDER BY dayofweek(order_time) ASC;
+
+-- 11 How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT 
+    EXTRACT(WEEK FROM pickup_time) + 1 weeks,
+    COUNT(DISTINCT runner_id) reg_runners,
+    COUNT(*) order_numbers
+FROM
+    runner_orders
+WHERE
+    cancellation IS NULL
+GROUP BY Weeks;
+
+-- 12 What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+SELECT 
+    runner_id,
+    COUNT(*) order_number,
+    ROUND(AVG(SUBSTRING_INDEX(duration, ' ', 1)),
+            0) average_d_time
+FROM
+    runner_orders
+WHERE
+    cancellation IS NULL
+GROUP BY runner_id;
+
+/** 13 Is there any relationship between the number of pizzas and how long the order takes to prepare - 
+ The more the pizza ordered, the more time it takes to prepare and it 
+ takes an average of 10 mins to prepare one pizza and about 30 mins for 3 pizzas? **/
+SELECT 
+    p.pickup_time,
+    c.order_time,
+     q.pizza_name,
+    c.order_id,
+    COUNT(c.order_id) Number_of_Orders,
+    TIME(AVG(TIMEDIFF(p.pickup_time, c.order_time))) prep_duration
+FROM
+    customer_orders c
+        LEFT JOIN
+    runner_orders p ON c.order_id = p.order_id
+        INNER JOIN
+    daily_pizza d ON c.pizza_serial_number = d.pizza_serial_number
+        INNER JOIN
+    pizza_names q ON q.pizza_id = d.pizza_id
+WHERE
+    p.cancellation IS NULL
+GROUP BY order_id;
+
+/** 14 What was the average distance travelled for each customer - 
+Customer distance travelled  104 had the lowest number of distance covered though
+he purchased the highest item **/
+SELECT 
+    c.customer_id,
+    ROUND(AVG(SUBSTRING_INDEX(p.distance, ' ', 1)),
+            0) Distance
+FROM
+    customer_orders c
+        LEFT JOIN
+    runner_orders p ON c.order_id = p.order_id
+WHERE
+    p.cancellation IS NULL
+GROUP BY c.customer_id;
+
+-- 15 What was the difference between the longest and shortest delivery times for all orders?
+
+SELECT 
+    MIN(ROUND((SUBSTRING_INDEX(distance, ' ', 1)), 0)) Min_distance,
+    MAX(ROUND((SUBSTRING_INDEX(distance, ' ', 1)), 0)) Max_distance,
+    (MAX(ROUND((SUBSTRING_INDEX(distance, ' ', 1)), 0)) - MIN(ROUND((SUBSTRING_INDEX(distance, ' ', 1)), 0))) Distance
+FROM
+    runner_orders
+WHERE
+    cancellation IS NULL;
+
+-- 16 What was the average speed for each runner for each delivery and do you notice any trend for these values?
+SELECT 
+    runner_id,
+    COUNT(runner_id) order_number,
+    ROUND(SUM(SUBSTRING_INDEX(distance, ' ', 1)),
+            0) * 1000 `Distance(m)`,
+    ROUND(SUM(SUBSTRING_INDEX(duration, ' ', 1)),
+            0) * 60 `time_taken(s)`,
+    ROUND(((ROUND(SUM(SUBSTRING_INDEX(distance, ' ', 1)),
+                    0) * 1000) / (ROUND(SUM(SUBSTRING_INDEX(duration, ' ', 1)),
+                    0) * 60)),
+            1) `speed(m/s)`
+FROM
+    runner_orders
+WHERE
+    cancellation IS NULL
+ GROUP BY runner_id ;
+
+-- 17 What is the successful delivery percentage for each runner?
+SELECT 
+    p.runner_id,
+    e.delivered all_request,
+    p.delivered,
+    concat((ROUND((p.delivered / e.delivered) * 100)),' %') `%`
+FROM
+    (SELECT 
+        runner_id, COUNT(1) AS delivered
+    FROM
+        runner_orders
+    WHERE
+        cancellation IS NULL
+    GROUP BY runner_id) AS p
+        INNER JOIN
+    (SELECT 
+        runner_id, COUNT(1) AS delivered
+    FROM
+        runner_orders
+    GROUP BY runner_id) AS e ON e.runner_id = p.runner_id;
+    
+
+-- Price and Ratings Review --
+
+-- 1 If a Meat Lovers pizza costs $12 and Vegetarian costs $10 and there were no charges for changes - how much money has Pizza Runner made so far if there are no delivery fees?
+SELECT 
+     if (grouping(q.pizza_name),"Total",q.pizza_name) `pizza name`,
+     if(pizza_name = 'Meatlovers', COUNT(c.order_id) * 12,COUNT(c.order_id) * 10)
+     `Pizza Amount`,
+    COUNT(c.order_id) `Number of Orders`
+FROM
+    customer_orders c
+        LEFT JOIN
+    runner_orders p ON c.order_id = p.order_id
+        INNER JOIN
+    daily_pizza d ON c.pizza_serial_number = d.pizza_serial_number
+        INNER JOIN
+    pizza_names q ON q.pizza_id = d.pizza_id
+WHERE
+    p.cancellation IS NULL
+GROUP BY q.pizza_name with ROLLUP;
+
+ 
+
+
